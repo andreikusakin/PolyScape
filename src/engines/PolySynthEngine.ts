4ba3661c99740ae9b2018c83b6c08d07c7e95c34
andreikusakin <andrewkusakin@me.com>
2024-01-17 00:01:55 -0500

    added methods for filter

--- src/engines/PolySynthEngine.ts
+++ src/engines/PolySynthEngine.ts
@@ -25,11 +25,12 @@ class PolySynthEngine {
   private OSC1Transpose: number = 0;
   private OSC2Transpose: number = 0;
   private OSC1Panners: Tone.Panner[];
-  private OSC2Panners: Tone.Panner[];
-  private OSC1Filters: Tone.Filter[];
-  private OSC2Filters: Tone.Filter[];
+  private OSC2Panners: Tone.Panner[];;
   private keyboard: AudioKeys;
   private unison: boolean = false;
+  private phaseLFO: Tone.LFO;
+  private lastPlayedVoiceOSC1: number = 0;
+private lastPlayedVoiceOSC2: number = 0;
 
   constructor(node: Tone.Gain<"decibels" | "gain" | "normalRange">) {
     this.OSC1Voices = [];
@@ -41,6 +42,12 @@ class PolySynthEngine {
     this.activeVoicesOSC1 = new Map();
     this.activeVoicesOSC2 = new Map();
     this.unison;
+    this.phaseLFO = new Tone.LFO({
+      type: "sine",
+      min: -0.1,
+      max: 0.1,
+      frequency: 0.3,
+    });
 
     for (let i = 0; i < this.voiceCount; i++) {
       this.OSC1Panners.push(new Tone.Panner().connect(node));
@@ -48,20 +55,42 @@ class PolySynthEngine {
         new Tone.MonoSynth({
           oscillator: { type: "sine" },
           envelope: { attack: 0.01, decay: 0.01, sustain: 1, release: 0.01 },
-          filter: { type: "lowpass", frequency: 20000, rolloff: -12, Q: 0 },
-          filterEnvelope: { attack: 0.01, decay: 0.01, sustain: 1, release: 0.01, baseFrequency: 20000, octaves: 4, exponent: 0 }
+          filter: { type: "lowpass", frequency: 0, rolloff: -12, Q: 0 },
+          filterEnvelope: {
+            attack: 0,
+            decay: 0,
+            sustain: 0,
+            release: 0,
+            baseFrequency: 15000,
+            octaves: 0,
+            exponent: 1,
+          },
         }).connect(this.OSC1Panners[i])
       );
       this.OSC2Panners.push(new Tone.Panner().connect(node));
       this.OSC2Voices.push(
         new Tone.MonoSynth({
-          oscillator: { type: "sine" },
+          oscillator: { type: "sine", phase: 0 },
           envelope: { attack: 0.01, decay: 0.01, sustain: 1, release: 0.01 },
-          filter: { type: "lowpass", frequency: 20000, rolloff: -12, Q: 0 },
-          filterEnvelope: { attack: 0.01, decay: 0.01, sustain: 1, release: 0.01, baseFrequency: 20000, octaves: 4, exponent: 0 }
+          filter: { type: "lowpass", frequency: 0, rolloff: -12, Q: 0 },
+          filterEnvelope: {
+            attack: 0,
+            decay: 0,
+            sustain: 0,
+            release: 0,
+            baseFrequency: 15000,
+            octaves: 0,
+            exponent: 1,
+          },
+          detune: 0,
         }).connect(this.OSC2Panners[i])
       );
     }
+    this.phaseLFO.start();
+    this.OSC2Voices.forEach((v) => {
+      this.phaseLFO.connect(v.detune);
+      this.phaseLFO.connect(v.volume);
+    });
 
     this.keyboard = new AudioKeys({
       polyphony: this.voiceCount,
@@ -80,26 +109,23 @@ class PolySynthEngine {
 
   private triggerAttackEngine(frequency: number, time, velocity: number) {
     if (this.unison) {
-      this.OSC1Voices.forEach((v) =>
-        v.triggerAttack(frequency, time, velocity)
-      );
-      this.OSC2Voices.forEach((v) =>
-        v.triggerAttack(frequency, time, velocity)
-      );
+      this.OSC1Voices.forEach(v => v.triggerAttack(frequency, time, velocity));
+      this.OSC2Voices.forEach(v => v.triggerAttack(frequency, time, velocity));
     } else {
-      const OSC1Voice =
-        this.OSC1Voices.find((v) => !this.activeVoicesOSC1.has(v)) ||
-        this.OSC1Voices[0];
+      const nextVoiceIndexOSC1 = (this.lastPlayedVoiceOSC1 + 1) % this.OSC1Voices.length;
+      const OSC1Voice = this.OSC1Voices[nextVoiceIndexOSC1];
       OSC1Voice.triggerAttack(frequency, time, velocity);
       this.activeVoicesOSC1.set(OSC1Voice, frequency);
-
-      const OSC2Voice =
-        this.OSC2Voices.find((v) => !this.activeVoicesOSC2.has(v)) ||
-        this.OSC2Voices[0];
+      this.lastPlayedVoiceOSC1 = nextVoiceIndexOSC1;
+  
+      const nextVoiceIndexOSC2 = (this.lastPlayedVoiceOSC2 + 1) % this.OSC2Voices.length;
+      const OSC2Voice = this.OSC2Voices[nextVoiceIndexOSC2];
       OSC2Voice.triggerAttack(frequency, time, velocity);
       this.activeVoicesOSC2.set(OSC2Voice, frequency);
+      this.lastPlayedVoiceOSC2 = nextVoiceIndexOSC2;
     }
   }
+  
 
   private triggerReleaseEngine(frequency: number) {
     if (this.unison) {
@@ -140,7 +166,7 @@ class PolySynthEngine {
     for (let i = 0; i < this.OSC1Voices.length; i++) {
       const currentDetune = this.OSC1Voices[i].detune.value;
       const step = Math.round(currentDetune / 100);
-      
+
       const detunedValue =
         Math.floor(((Math.random() * 60 - 30) * detune) / 100) + step * 100;
       this.OSC1Voices[i].detune.value = detunedValue;
@@ -182,8 +208,6 @@ class PolySynthEngine {
       console.log(this.OSC2Voices[i].detune.value + " transposed");
     }
   }
-
-  
 
   //Set Pan Spread
 
@@ -227,12 +251,99 @@ class PolySynthEngine {
     );
   }
 
+  //Set Filter
+
+  setFilterTypeEngine(
+    filterType: "lowpass" | "highpass" | "bandpass" | "notch"
+  ) {
+    this.OSC1Voices.forEach((v) => v.set({ filter: { type: filterType } }));
+    this.OSC2Voices.forEach((v) => v.set({ filter: { type: filterType } }));
+  }
+
+  setFilterFrequencyEngine(newFrequency: number) {
+    this.OSC1Voices.forEach((v) =>
+      v.set({ filter: { frequency: newFrequency } })
+    );
+    console.log(newFrequency);
+    this.OSC2Voices.forEach((v) =>
+      v.set({ filter: { frequency: newFrequency } })
+    );
+  }
+
+  setFilterQEngine(newQ: number) {
+    this.OSC1Voices.forEach((v) => v.set({ filter: { Q: newQ } }));
+    this.OSC2Voices.forEach((v) => v.set({ filter: { Q: newQ } }));
+  }
+
+  setFilterRollOffEngine(newRollOff: Tone.FilterRollOff) {
+    this.OSC1Voices.forEach((v) => v.set({ filter: { rolloff: newRollOff } }));
+    this.OSC2Voices.forEach((v) => v.set({ filter: { rolloff: newRollOff } }));
+  }
+
   //Set Envelope Filter
 
+  setFilterEnvelopeAttackEngine(newAttack: number) {
+    this.OSC1Voices.forEach((v) =>
+      v.set({ filterEnvelope: { attack: newAttack } })
+    );
+    this.OSC2Voices.forEach((v) =>
+      v.set({ filterEnvelope: { attack: newAttack } })
+    );
+  }
 
-  //Set Filter
- 
+  setFilterEnvelopeDecayEngine(newDecay: number) {
+    this.OSC1Voices.forEach((v) =>
+      v.set({ filterEnvelope: { decay: newDecay } })
+    );
+    this.OSC2Voices.forEach((v) =>
+      v.set({ filterEnvelope: { decay: newDecay } })
+    );
+  }
 
+  setFilterEnvelopeSustainEngine(newSustain: number) {
+    this.OSC1Voices.forEach((v) =>
+      v.set({ filterEnvelope: { sustain: newSustain } })
+    );
+    this.OSC2Voices.forEach((v) =>
+      v.set({ filterEnvelope: { sustain: newSustain } })
+    );
+  }
+
+  setFilterEnvelopeReleaseEngine(newRelease: number) {
+    this.OSC1Voices.forEach((v) =>
+      v.set({ filterEnvelope: { release: newRelease } })
+    );
+    this.OSC2Voices.forEach((v) =>
+      v.set({ filterEnvelope: { release: newRelease } })
+    );
+  }
+
+  setFilterEnvelopeFrequencyEngine(newFrequency: number) {
+    this.OSC1Voices.forEach((v) =>
+      v.set({ filterEnvelope: { baseFrequency: newFrequency } })
+    );
+    this.OSC2Voices.forEach((v) =>
+      v.set({ filterEnvelope: { baseFrequency: newFrequency } })
+    );
+  }
+
+  setFilterEnvelopeExponentEngine(newExponent: number) {
+    this.OSC1Voices.forEach((v) =>
+      v.set({ filterEnvelope: { exponent: newExponent } })
+    );
+    this.OSC2Voices.forEach((v) =>
+      v.set({ filterEnvelope: { exponent: newExponent } })
+    );
+  }
+
+  setFilterEnvelopeOctavesEngine(newOctaves: number) {
+    this.OSC1Voices.forEach((v) =>
+      v.set({ filterEnvelope: { octaves: newOctaves } })
+    );
+    this.OSC2Voices.forEach((v) =>
+      v.set({ filterEnvelope: { octaves: newOctaves } })
+    );
+  }
 }
 
 export default PolySynthEngine;
