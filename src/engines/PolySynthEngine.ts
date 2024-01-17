998e25720be9f7a84aa12b7d6eee0577fcd4d2f8
andreikusakin <andrewkusakin@me.com>
2024-01-17 18:06:28 -0500

    Refactor code

--- src/engines/PolySynthEngine.ts
+++ src/engines/PolySynthEngine.ts
@@ -14,44 +14,43 @@ import AudioKeys from "audiokeys";
 // noise
 // midi and midi mapping
 
+//combine methods for both oscs
+
 //mimick detune and panspread
+
+//mixer mimick
 
 class PolySynthEngine {
   private voiceCount: number = 8;
-  private OSC1Voices: Tone.MonoSynth[];
-  private OSC2Voices: Tone.MonoSynth[];
-  private activeVoicesOSC1: Map<Tone.MonoSynth, number>;
-  private activeVoicesOSC2: Map<Tone.MonoSynth, number>;
-  private OSC1Transpose: number = 0;
-  private OSC2Transpose: number = 0;
-  private OSC1Panners: Tone.Panner[];
-  private OSC2Panners: Tone.Panner[];;
+  private voices: [Tone.MonoSynth, Tone.MonoSynth][];
+  private panners: Tone.Panner[];
+  private activeVoices: Map<number, number>;
+  private lastPlayedVoice: number = 0;
   private keyboard: AudioKeys;
   private unison: boolean = false;
-  private phaseLFO: Tone.LFO;
-  private lastPlayedVoiceOSC1: number = 0;
-private lastPlayedVoiceOSC2: number = 0;
+  // private phaseLFO: Tone.LFO;
+
+  private currentDetuneOSC1Values: number[] = [0, 0, 0, 0, 0, 0, 0, 0];
+  private currentDetuneOSC2Values: number[] = [0, 0, 0, 0, 0, 0, 0, 0];
+  private maxDetuneOSC1: number[] = [31, -49, 49, -45, 24, -40, 42, -32];
+  private maxDetuneOSC2: number[] = [-41, 38, -25, 49, 15, -49, 18, 39];
 
   constructor(node: Tone.Gain<"decibels" | "gain" | "normalRange">) {
-    this.OSC1Voices = [];
-    this.OSC2Voices = [];
-    this.OSC1Panners = [];
-    this.OSC2Panners = [];
-    this.OSC1Transpose;
-    this.OSC2Transpose;
-    this.activeVoicesOSC1 = new Map();
-    this.activeVoicesOSC2 = new Map();
+    this.voices = [];
+    this.panners = [];
+    this.activeVoices = new Map();
+
     this.unison;
-    this.phaseLFO = new Tone.LFO({
-      type: "sine",
-      min: -0.1,
-      max: 0.1,
-      frequency: 0.3,
-    });
+    // this.phaseLFO = new Tone.LFO({
+    //   type: "sine",
+    //   min: -0.1,
+    //   max: 0.1,
+    //   frequency: 0.3,
+    // });
 
     for (let i = 0; i < this.voiceCount; i++) {
-      this.OSC1Panners.push(new Tone.Panner().connect(node));
-      this.OSC1Voices.push(
+      this.panners.push(new Tone.Panner().connect(node));
+      this.voices.push([
         new Tone.MonoSynth({
           oscillator: { type: "sine" },
           envelope: { attack: 0.01, decay: 0.01, sustain: 1, release: 0.01 },
@@ -65,10 +64,7 @@ private lastPlayedVoiceOSC2: number = 0;
             octaves: 0,
             exponent: 1,
           },
-        }).connect(this.OSC1Panners[i])
-      );
-      this.OSC2Panners.push(new Tone.Panner().connect(node));
-      this.OSC2Voices.push(
+        }).connect(this.panners[i]),
         new Tone.MonoSynth({
           oscillator: { type: "sine", phase: 0 },
           envelope: { attack: 0.01, decay: 0.01, sustain: 1, release: 0.01 },
@@ -83,14 +79,11 @@ private lastPlayedVoiceOSC2: number = 0;
             exponent: 1,
           },
           detune: 0,
-        }).connect(this.OSC2Panners[i])
-      );
+        }).connect(this.panners[i]),
+      ]);
     }
-    this.phaseLFO.start();
-    this.OSC2Voices.forEach((v) => {
-      this.phaseLFO.connect(v.detune);
-      this.phaseLFO.connect(v.volume);
-    });
+
+    console.log(this.voices);
 
     this.keyboard = new AudioKeys({
       polyphony: this.voiceCount,
@@ -109,40 +102,34 @@ private lastPlayedVoiceOSC2: number = 0;
 
   private triggerAttackEngine(frequency: number, time, velocity: number) {
     if (this.unison) {
-      this.OSC1Voices.forEach(v => v.triggerAttack(frequency, time, velocity));
-      this.OSC2Voices.forEach(v => v.triggerAttack(frequency, time, velocity));
+      this.voices.forEach(([osc1, osc2]) => {
+        osc1.triggerAttack(frequency, time, velocity);
+        osc2.triggerAttack(frequency, time, velocity);
+      });
     } else {
-      const nextVoiceIndexOSC1 = (this.lastPlayedVoiceOSC1 + 1) % this.OSC1Voices.length;
-      const OSC1Voice = this.OSC1Voices[nextVoiceIndexOSC1];
-      OSC1Voice.triggerAttack(frequency, time, velocity);
-      this.activeVoicesOSC1.set(OSC1Voice, frequency);
-      this.lastPlayedVoiceOSC1 = nextVoiceIndexOSC1;
-  
-      const nextVoiceIndexOSC2 = (this.lastPlayedVoiceOSC2 + 1) % this.OSC2Voices.length;
-      const OSC2Voice = this.OSC2Voices[nextVoiceIndexOSC2];
-      OSC2Voice.triggerAttack(frequency, time, velocity);
-      this.activeVoicesOSC2.set(OSC2Voice, frequency);
-      this.lastPlayedVoiceOSC2 = nextVoiceIndexOSC2;
+      this.lastPlayedVoice = (this.lastPlayedVoice + 1) % this.voiceCount;
+      const voicePair = this.voices[this.lastPlayedVoice];
+      const [osc1, osc2] = voicePair;
+      osc1.triggerAttack(frequency, time, velocity);
+      osc2.triggerAttack(frequency, time, velocity);
+      this.activeVoices.set(this.lastPlayedVoice, frequency);
     }
   }
-  
 
   private triggerReleaseEngine(frequency: number) {
     if (this.unison) {
-      this.OSC1Voices.forEach((v) => v.triggerRelease());
-      this.OSC2Voices.forEach((v) => v.triggerRelease());
-    } else {
-      this.activeVoicesOSC1.forEach((freq, voice) => {
-        if (freq === frequency) {
-          voice.triggerRelease();
-          this.activeVoicesOSC1.delete(voice);
-        }
+      this.voices.forEach(([osc1, osc2]) => {
+        osc1.triggerRelease();
+        osc2.triggerRelease();
       });
-
-      this.activeVoicesOSC2.forEach((freq, voice) => {
+    } else {
+      this.activeVoices.forEach((freq, voiceIndex) => {
         if (freq === frequency) {
-          voice.triggerRelease();
-          this.activeVoicesOSC2.delete(voice);
+          const voicePair = this.voices[voiceIndex];
+          const [osc1, osc2] = voicePair;
+          osc1.triggerRelease();
+          osc2.triggerRelease();
+          this.activeVoices.delete(voiceIndex);
         }
       });
     }
@@ -151,104 +138,101 @@ private lastPlayedVoiceOSC2: number = 0;
   //Set Waveforms
 
   setOsc1TypeEngine(waveform: "sawtooth" | "sine" | "square" | "triangle") {
-    this.OSC1Voices.forEach((v: Tone.MonoSynth) =>
-      v.set({ oscillator: { type: waveform } })
+    this.voices.forEach(([osc1, osc2]) =>
+      osc1.set({ oscillator: { type: waveform } })
     );
   }
 
   setOsc2TypeEngine(waveform: "sawtooth" | "sine" | "square" | "triangle") {
-    this.OSC2Voices.forEach((v) => v.set({ oscillator: { type: waveform } }));
+    this.voices.forEach(([osc1, osc2]) =>
+      osc2.set({ oscillator: { type: waveform } })
+    );
   }
 
   //Set Detune
 
-  setOsc1DetuneEngine(detune: number) {
-    for (let i = 0; i < this.OSC1Voices.length; i++) {
-      const currentDetune = this.OSC1Voices[i].detune.value;
+  setOsc1DetuneEngine(detune: number, osc: number) {
+    for (let i = 0; i < this.voiceCount; i++) {
+      const maxDetune = this.maxDetuneOSC1[i];
+      const currentDetune = this.voices[i][0].detune.value;
       const step = Math.round(currentDetune / 100);
-
-      const detunedValue =
-        Math.floor(((Math.random() * 60 - 30) * detune) / 100) + step * 100;
-      this.OSC1Voices[i].detune.value = detunedValue;
-      console.log(this.OSC1Voices[i].detune.value + " detuned value OSC1");
+      const detuneAmount = maxDetune * (detune / 100);
+      this.currentDetuneOSC1Values[i] = detuneAmount;
+      this.voices[i][0].detune.value = detuneAmount + step * 100;
+      console.log(this.voices[i][0].detune.value + " detuned value OSC1");
     }
   }
 
   setOsc2DetuneEngine(detune: number) {
-    for (let i = 0; i < this.OSC2Voices.length; i++) {
-      const currentDetune = this.OSC2Voices[i].detune.value;
+    for (let i = 0; i < this.voiceCount; i++) {
+      const maxDetune = this.maxDetuneOSC2[i];
+      const currentDetune = this.voices[i][1].detune.value;
       const step = Math.round(currentDetune / 100);
-      const detunedValue =
-        Math.floor(((Math.random() * 60 - 30) * detune) / 100) + step * 100;
-      this.OSC2Voices[i].detune.value = detunedValue;
-      console.log(this.OSC1Voices[i].detune.value + " detuned value OSC2");
+      const detuneAmount = maxDetune * (detune / 100);
+      this.currentDetuneOSC2Values[i] = detuneAmount;
+      this.voices[i][1].detune.value = detuneAmount + step * 100;
+      console.log(this.voices[i][1].detune.value + " detuned value OSC2");
     }
   }
 
   //Set Transpose
 
   setOsc1TransposeEngine(transpose: number) {
-    for (let i = 0; i < this.OSC1Voices.length; i++) {
-      const currentDetune = this.OSC1Voices[i].detune.value;
-      const detune = currentDetune % 100;
-      console.log("CURRENT DETUNE " + currentDetune);
-      const transposedValue = transpose * 100 + detune;
-      this.OSC1Voices[i].detune.value = transposedValue;
-      console.log(this.OSC1Voices[i].detune.value + " transposed");
+    for (let i = 0; i < this.voices.length; i++) {
+      // const currentDetune = this.voices[i][0].detune.value % 100;
+      // console.log("CURRENT DETUNE " + currentDetune);
+      const transposedValue = transpose * 100 + this.currentDetuneOSC1Values[i];
+      this.voices[i][0].detune.value = transposedValue;
+      // console.log(
+      //   "OSC1 transposed by " + transpose + " semitones"
+      // );
     }
   }
 
-  setOsc2TransposeEngine(transpose: number) {
-    for (let i = 0; i < this.OSC2Voices.length; i++) {
-      const currentDetune = this.OSC2Voices[i].detune.value;
+  setOsc2TransposeEngine(transpose: number ) {
+    for (let i = 0; i < this.voices.length; i++) {
+      const currentDetune = this.voices[i].detune.value;
       const detune = currentDetune % 100;
       console.log("CURRENT DETUNE " + currentDetune);
       const transposedValue = transpose * 100 + detune;
-      this.OSC2Voices[i].detune.value = transposedValue;
-      console.log(this.OSC2Voices[i].detune.value + " transposed");
+      this.voices[i].detune.value = transposedValue;
+      console.log(this.voices[i].detune.value + " transposed");
     }
   }
 
   //Set Pan Spread
 
   setPanSpreadEngine(panSpread: number) {
-    for (let i = 0; i < this.OSC1Panners.length; i++) {
-      const randomNumber1 = (Math.random() * 2 - 1) * (panSpread / 100);
-      const randomNumber2 = (Math.random() * 2 - 1) * (panSpread / 100);
-      console.log(randomNumber1);
-      this.OSC1Panners[i].pan.linearRampTo(randomNumber1, 0.01);
-      this.OSC2Panners[i].pan.linearRampTo(randomNumber2, 0.01);
+    for (let i = 0; i < this.voiceCount; i++) {
+      const randomNumber = (Math.random() * 2 - 1) * (panSpread / 100);
+
+      // this.OSC1Panners[i].pan.linearRampTo(randomNumber1, 0.01);
+      // this.OSC2Panners[i].pan.linearRampTo(randomNumber2, 0.01);
+
+      this.panners[i].pan.linearRampTo(randomNumber, 0.01);
     }
   }
 
   //Set Envelope Amplitude
 
   setAttackEngine(newAttack: number) {
-    this.OSC1Voices.forEach((v) => v.set({ envelope: { attack: newAttack } }));
-    this.OSC2Voices.forEach((v) => v.set({ envelope: { attack: newAttack } }));
+    this.voices.forEach((v) => v.set({ envelope: { attack: newAttack } }));
+    this.voices.forEach((v) => v.set({ envelope: { attack: newAttack } }));
   }
 
   setDecayEngine(newDecay: number) {
-    this.OSC1Voices.forEach((v) => v.set({ envelope: { decay: newDecay } }));
-    this.OSC2Voices.forEach((v) => v.set({ envelope: { decay: newDecay } }));
+    this.voices.forEach((v) => v.set({ envelope: { decay: newDecay } }));
+    this.voices.forEach((v) => v.set({ envelope: { decay: newDecay } }));
   }
 
   setSustainEngine(newSustain: number) {
-    this.OSC1Voices.forEach((v) =>
-      v.set({ envelope: { sustain: newSustain } })
-    );
-    this.OSC2Voices.forEach((v) =>
-      v.set({ envelope: { sustain: newSustain } })
-    );
+    this.voices.forEach((v) => v.set({ envelope: { sustain: newSustain } }));
+    this.voices.forEach((v) => v.set({ envelope: { sustain: newSustain } }));
   }
 
   setReleaseEngine(newRelease: number) {
-    this.OSC1Voices.forEach((v) =>
-      v.set({ envelope: { release: newRelease } })
-    );
-    this.OSC2Voices.forEach((v) =>
-      v.set({ envelope: { release: newRelease } })
-    );
+    this.voices.forEach((v) => v.set({ envelope: { release: newRelease } }));
+    this.voices.forEach((v) => v.set({ envelope: { release: newRelease } }));
   }
 
   //Set Filter
@@ -256,91 +240,83 @@ private lastPlayedVoiceOSC2: number = 0;
   setFilterTypeEngine(
     filterType: "lowpass" | "highpass" | "bandpass" | "notch"
   ) {
-    this.OSC1Voices.forEach((v) => v.set({ filter: { type: filterType } }));
-    this.OSC2Voices.forEach((v) => v.set({ filter: { type: filterType } }));
+    this.voices.forEach((v) => v.set({ filter: { type: filterType } }));
+    this.voices.forEach((v) => v.set({ filter: { type: filterType } }));
   }
 
   setFilterFrequencyEngine(newFrequency: number) {
-    this.OSC1Voices.forEach((v) =>
-      v.set({ filter: { frequency: newFrequency } })
-    );
+    this.voices.forEach((v) => v.set({ filter: { frequency: newFrequency } }));
     console.log(newFrequency);
-    this.OSC2Voices.forEach((v) =>
-      v.set({ filter: { frequency: newFrequency } })
-    );
+    this.voices.forEach((v) => v.set({ filter: { frequency: newFrequency } }));
   }
 
   setFilterQEngine(newQ: number) {
-    this.OSC1Voices.forEach((v) => v.set({ filter: { Q: newQ } }));
-    this.OSC2Voices.forEach((v) => v.set({ filter: { Q: newQ } }));
+    this.voices.forEach((v) => v.set({ filter: { Q: newQ } }));
+    this.voices.forEach((v) => v.set({ filter: { Q: newQ } }));
   }
 
   setFilterRollOffEngine(newRollOff: Tone.FilterRollOff) {
-    this.OSC1Voices.forEach((v) => v.set({ filter: { rolloff: newRollOff } }));
-    this.OSC2Voices.forEach((v) => v.set({ filter: { rolloff: newRollOff } }));
+    this.voices.forEach((v) => v.set({ filter: { rolloff: newRollOff } }));
+    this.voices.forEach((v) => v.set({ filter: { rolloff: newRollOff } }));
   }
 
   //Set Envelope Filter
 
   setFilterEnvelopeAttackEngine(newAttack: number) {
-    this.OSC1Voices.forEach((v) =>
+    this.voices.forEach((v) =>
       v.set({ filterEnvelope: { attack: newAttack } })
     );
-    this.OSC2Voices.forEach((v) =>
+    this.voices.forEach((v) =>
       v.set({ filterEnvelope: { attack: newAttack } })
     );
   }
 
   setFilterEnvelopeDecayEngine(newDecay: number) {
-    this.OSC1Voices.forEach((v) =>
-      v.set({ filterEnvelope: { decay: newDecay } })
-    );
-    this.OSC2Voices.forEach((v) =>
-      v.set({ filterEnvelope: { decay: newDecay } })
-    );
+    this.voices.forEach((v) => v.set({ filterEnvelope: { decay: newDecay } }));
+    this.voices.forEach((v) => v.set({ filterEnvelope: { decay: newDecay } }));
   }
 
   setFilterEnvelopeSustainEngine(newSustain: number) {
-    this.OSC1Voices.forEach((v) =>
+    this.voices.forEach((v) =>
       v.set({ filterEnvelope: { sustain: newSustain } })
     );
-    this.OSC2Voices.forEach((v) =>
+    this.voices.forEach((v) =>
       v.set({ filterEnvelope: { sustain: newSustain } })
     );
   }
 
   setFilterEnvelopeReleaseEngine(newRelease: number) {
-    this.OSC1Voices.forEach((v) =>
+    this.voices.forEach((v) =>
       v.set({ filterEnvelope: { release: newRelease } })
     );
-    this.OSC2Voices.forEach((v) =>
+    this.voices.forEach((v) =>
       v.set({ filterEnvelope: { release: newRelease } })
     );
   }
 
   setFilterEnvelopeFrequencyEngine(newFrequency: number) {
-    this.OSC1Voices.forEach((v) =>
+    this.voices.forEach((v) =>
       v.set({ filterEnvelope: { baseFrequency: newFrequency } })
     );
-    this.OSC2Voices.forEach((v) =>
+    this.voices.forEach((v) =>
       v.set({ filterEnvelope: { baseFrequency: newFrequency } })
     );
   }
 
   setFilterEnvelopeExponentEngine(newExponent: number) {
-    this.OSC1Voices.forEach((v) =>
+    this.voices.forEach((v) =>
       v.set({ filterEnvelope: { exponent: newExponent } })
     );
-    this.OSC2Voices.forEach((v) =>
+    this.voices.forEach((v) =>
       v.set({ filterEnvelope: { exponent: newExponent } })
     );
   }
 
   setFilterEnvelopeOctavesEngine(newOctaves: number) {
-    this.OSC1Voices.forEach((v) =>
+    this.voices.forEach((v) =>
       v.set({ filterEnvelope: { octaves: newOctaves } })
     );
-    this.OSC2Voices.forEach((v) =>
+    this.voices.forEach((v) =>
       v.set({ filterEnvelope: { octaves: newOctaves } })
     );
   }
