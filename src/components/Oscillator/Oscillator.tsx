ca874a6226665d14ada31ecb1ae1a5c86dc10e68
andreikusakin <andrewkusakin@me.com>
2024-01-22 18:05:15 -0500

    moved waveform shapes into separate file

--- src/components/Oscillator/Oscillator.tsx
+++ src/components/Oscillator/Oscillator.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Knob from "../Knob/Knob";
 import styles from "./Oscillator.module.css";
+import { Sawtooth, Sine, Square, Triangle } from "./Shapes";
 
 type OscillatorProps = {
   name: string;
@@ -8,6 +9,21 @@ type OscillatorProps = {
   setOscillatorType: (
     oscType: "sine" | "sawtooth" | "pulse" | "triangle"
   ) => void;
+};
+
+const OscillatorWaveform = ({ oscType }: { oscType: "sine" | "sawtooth" | "pulse" | "triangle" }) => {
+  switch (oscType) {
+    case "sine":
+      return <Sine />;
+    case "sawtooth":
+      return <Sawtooth />;
+    case "pulse":
+      return <Square />;
+    case "triangle":
+      return <Triangle />;
+    default:
+      return null;
+  }
 };
 
 const Oscillator: React.FC<OscillatorProps> = ({
@@ -19,77 +35,70 @@ const Oscillator: React.FC<OscillatorProps> = ({
     <div className={styles.oscillator}>
       <div className={styles.oscname}>{name}</div>
       <div className={styles.container}>
-        <div className={styles.row}>
-          <ul className={styles.shapes}>
-            <li
-              onClick={() => setOscillatorType("sine")}
-              className={oscType === "sine" ? styles.selected : ""}
-            >
-              sine
-            </li>
-            <li
-              onClick={() => setOscillatorType("sawtooth")}
-              className={oscType === "sawtooth" ? styles.selected : ""}
-            >
-              sawtooth
-            </li>
-            <li
-              onClick={() => setOscillatorType("pulse")}
-              className={oscType === "pulse" ? styles.selected : ""}
-            >
-              square
-            </li>
-            <li
-              onClick={() => setOscillatorType("triangle")}
-              className={oscType === "triangle" ? styles.selected : ""}
-            >
-              triangle
-            </li>
-          </ul>
-          <div className={styles.waveform}>
-            <svg width="22" height="22" viewBox="0 0 22 22" fill="none">
-              <path
-                d="M1 22L0.999999 0M11 22L11 0M21 22L21 0M0 1H12M10 21H22"
-                stroke="#86FFD3"
-                stroke-width="2"
-              />
-            </svg>
-          </div>
-          <Knob
-            name={"pulse width"}
-            radius={24}
-            percent={45}
-            lfo={1}
-            startingPoint={"middle"}
-            interactive={false}
-          />
+        <ul className={styles.shapes}>
+          <li
+            onClick={() => setOscillatorType("sine")}
+            className={oscType === "sine" ? styles.selected : ""}
+          >
+            sine
+          </li>
+          <li
+            onClick={() => setOscillatorType("sawtooth")}
+            className={oscType === "sawtooth" ? styles.selected : ""}
+          >
+            sawtooth
+          </li>
+          <li
+            onClick={() => setOscillatorType("pulse")}
+            className={oscType === "pulse" ? styles.selected : ""}
+          >
+            square
+          </li>
+          <li
+            onClick={() => setOscillatorType("triangle")}
+            className={oscType === "triangle" ? styles.selected : ""}
+          >
+            triangle
+          </li>
+        </ul>
+
+        <div className={styles.waveform}>
+          <OscillatorWaveform oscType={oscType} />
         </div>
-        <div className={styles.row}>
-          <Knob
-            name={"coarse"}
-            radius={24}
-            percent={45}
-            lfo={1}
-            startingPoint={"middle"}
-            interactive={true}
-          />
-          <Knob
-            name={"detune"}
-            radius={24}
-            percent={45}
-            lfo={1}
-            startingPoint={"middle"}
-            interactive={true}
-          />
-          <Knob
-            name={"volume"}
-            radius={24}
-            percent={45}
-            lfo={1}
-            startingPoint={"middle"}
-            interactive={true}
-          />
-        </div>
+        <Knob
+          label={"pulse width"}
+          radius={24}
+          percent={50}
+          lfo={1}
+          lfoPercent={100}
+          startingPoint={"middle"}
+          interactive={oscType === "pulse" ? true : false}
+        />
+
+        <Knob
+          label={"coarse"}
+          radius={24}
+          percent={45}
+          lfo={1}
+          startingPoint={"middle"}
+          interactive={true}
+        />
+        <Knob
+          label={"detune"}
+          radius={24}
+          percent={45}
+          lfo={1}
+          startingPoint={"middle"}
+          interactive={true}
+        />
+        <Knob
+          label={"volume"}
+          radius={24}
+          percent={45}
+          lfo={1}
+          startingPoint={"middle"}
+          interactive={true}
+        />
       </div>
     </div>
   );
---
+++ src/components/Oscillator/Shapes.tsx
@@ -0,0 +1,79 @@
+export const Sine = () => {
+  return (
+    <svg
+      width="92"
+      height="26"
+      viewBox="0 0 92 26"
+      fill="none"
+      filter="drop-shadow(0px 0px 4px #FF543D"
+    >
+      <path
+        d="M1 13C3.25 7 5.5 1 8.5 1C14.5 1 17.5 25 23.5 25C29.5 25 32.5 1 38.5 1C44.5 1 47.5 25 53.5 25C59.5 25 62.5 1 68.5 1C74.5 1 77.5 25 83.5 25C86.5 25 88.75 19 91 13"
+        stroke="#FF543D"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+      />
+    </svg>
+  );
+};
+
+export const Sawtooth = () => {
+  return (
+    <svg
+      width="92"
+      height="26"
+      viewBox="0 0 92 26"
+      fill="none"
+      filter="drop-shadow(0px 0px 4px #49FF92"
+    >
+      <path
+        d="M1 13L16 1V25L46 1V25L76 1V25L91 13"
+        stroke="#49FF92"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+      />
+    </svg>
+  );
+};
+
+export const Square = () => {
+  return (
+    <svg
+      width="92"
+      height="26"
+      viewBox="0 0 92 26"
+      fill="none"
+      filter="drop-shadow(0px 0px 4px #2A99FF"
+    >
+      <path
+        d="M1 1H16V25H31V1H46V25H61V1H76V25H91"
+        stroke="#2A99FF"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+      />
+    </svg>
+  );
+};
+
+export const Triangle = () => {
+  return (
+    <svg
+      width="92"
+      height="26"
+      viewBox="0 0 92 26"
+      fill="none"
+      filter="drop-shadow(0px 0px 4px #E859FF"
+    >
+      <path
+        d="M1 13L8.5 1L23.5 25L38.5 1L53.5 25L68.5 1L83.5 25L91 13"
+        stroke="#E859FF"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+      />
+    </svg>
+  );
+};
