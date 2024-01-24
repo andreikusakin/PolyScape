61e75b04a7e768b38bb97a2872b282738dbec561
andreikusakin <andrewkusakin@me.com>
2024-01-23 23:47:20 -0500

    added noise types to preset

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -1,44 +1,47 @@
 import * as Tone from "tone/build/esm/index";
 
 export type Preset = {
-    osc1: {
-      type: "sawtooth" | "sine" | "pulse" | "triangle";
-      detune: number;
-      transpose: number;
-      pulseWidth: number;
-      volume: number;
-    };
-    osc2: {
-      type: "sawtooth" | "sine" | "pulse" | "triangle";
-      detune: number;
-      transpose: number;
-      pulseWidth: number;
-      volume: number;
-    };
-    envelope: {
-      attack: number;
-      decay: number;
-      sustain: number;
-      release: number;
-    };
-    filter: {
-      type: "lowpass" | "highpass" | "bandpass" | "notch";
-      frequency: number;
-      rolloff: Tone.FilterRollOff;
-      Q: number;
-    };
-    filterEnvelope: {
-      attack: number;
-      decay: number;
-      sustain: number;
-      release: number;
-      baseFrequency: number;
-      octaves: number;
-      exponent: number;
-    };
-    unison: boolean;
-    panSpread: number;
+  osc1: {
+    type: "sawtooth" | "sine" | "pulse" | "triangle";
+    detune: number;
+    transpose: number;
+    pulseWidth: number;
+    volume: number;
   };
-  
+  osc2: {
+    type: "sawtooth" | "sine" | "pulse" | "triangle";
+    detune: number;
+    transpose: number;
+    pulseWidth: number;
+    volume: number;
+  };
+  noise: {
+    type: "white" | "pink" | "brown";
+    volume: number;
+  };
+  envelope: {
+    attack: number;
+    decay: number;
+    sustain: number;
+    release: number;
+  };
+  filter: {
+    type: "lowpass" | "highpass" | "bandpass" | "notch";
+    frequency: number;
+    rolloff: Tone.FilterRollOff;
+    Q: number;
+  };
+  filterEnvelope: {
+    attack: number;
+    decay: number;
+    sustain: number;
+    release: number;
+    baseFrequency: number;
+    octaves: number;
+    exponent: number;
+  };
+  unison: boolean;
+  panSpread: number;
+};
+
 export type LFOTarget = "osc1Detune" | "osc2Detune" | "filter" | "attack";
-  
