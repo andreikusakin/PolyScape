a95ebd3f7909306462b6332ea4bdf8809a75d8ac
andreikusakin <andrewkusakin@me.com>
2024-02-14 00:33:06 -0500

    Add LFO engine and implement assign and double click functionality

--- src/components/LFO/LFO.tsx
+++ src/components/LFO/LFO.tsx
@@ -3,18 +3,23 @@ import * as Tone from "tone/build/esm/index";
 import styles from "./LFO.module.css";
 import Knob from "../Knob/Knob";
 import { OscillatorWaveform } from "../OscillatorWaveform";
+import CustomPolySynth from "@/lib/engines/CustomPolySynth";
 
 type LFOProps = {
+  engine?: CustomPolySynth;
   name: string;
   type: "sine" | "triangle" | "sawtooth" | "square";
   setType: (type: "sine" | "triangle" | "sawtooth" | "square") => void;
   rate: Tone.Unit.Frequency;
   setRate: (rate: Tone.Unit.Frequency) => void;
   destinations: [];
-  setDestinations: (destinations: LFOTarget[]) => void;
+  setDestinations: ([]) => void;
+  isSelecting: false | 1 | 2;
+  setIsSelecting: (lfo: false | 1 | 2) => void;
 };
 
 export const LFO = ({
+  engine,
   name,
   type,
   setType,
@@ -22,7 +27,36 @@ export const LFO = ({
   setRate,
   destinations,
   setDestinations,
+  isSelecting,
+  setIsSelecting,
 }: LFOProps) => {
+  const handleAssignClick = () => {
+    setIsSelecting(name === "lfo1" ? 1 : 2);
+  };
+
+  const updateWaveformType = (
+    type: "sine" | "sawtooth" | "square" | "triangle"
+  ) => {
+    setType(type);
+    name === "lfo1"
+      ? engine?.LFO1.forEach((lfo) => (lfo.LFO.type = type))
+      : engine?.LFO2.forEach((lfo) => (lfo.LFO.type = type));
+  };
+
+  const handleDoubleClick = (target: LFOTarget) => {
+    if (name === "lfo1") {
+      setDestinations([...destinations.filter((d: LFODestination) => d.target !== target)]);
+      engine?.LFO1.forEach(lfo => {
+        if (lfo.target === target) {
+          lfo.LFO.stop();
+          lfo.LFO.disconnect();
+        }});
+    const filteredLFOs = engine?.LFO1.filter(lfo => lfo.target !== target);
+    if (engine)
+    engine.LFO1 = filteredLFOs ?? [];
+    }
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.name}>{name}</div>
@@ -30,25 +64,25 @@ export const LFO = ({
         <div>
           <ul className={styles.shapes}>
             <li
-              onClick={() => setType("sine")}
+              onClick={() => updateWaveformType("sine")}
               className={type === "sine" ? styles.selected : ""}
             >
               sine
             </li>
             <li
-              onClick={() => setType("sawtooth")}
+              onClick={() => updateWaveformType("sawtooth")}
               className={type === "sawtooth" ? styles.selected : ""}
             >
               sawtooth
             </li>
             <li
-              onClick={() => setType("square")}
+              onClick={() => updateWaveformType("square")}
               className={type === "square" ? styles.selected : ""}
             >
               square
             </li>
             <li
-              onClick={() => setType("triangle")}
+              onClick={() => updateWaveformType("triangle")}
               className={type === "triangle" ? styles.selected : ""}
             >
               triangle
@@ -65,14 +99,22 @@ export const LFO = ({
         </div>
         <div className={styles.destinations}>
           <div className={styles.underlay}>
-            <div className={styles.selector}><label>assign</label></div>
-            <div className={styles.selector}><label>assign</label></div>
-            <div className={styles.selector}><label>assign</label></div>
-            <div className={styles.selector}><label>assign</label></div>
+            <div className={styles.selector} onClick={handleAssignClick}>
+              <label>assign</label>
+            </div>
+            <div className={styles.selector} onClick={handleAssignClick}>
+              <label>assign</label>
+            </div>
+            <div className={styles.selector} onClick={handleAssignClick}>
+              <label>assign</label>
+            </div>
+            <div className={styles.selector} onClick={handleAssignClick}>
+              <label>assign</label>
+            </div>
           </div>
           <div className={styles.targets}>
             {destinations.map((d, i) => (
-              <div className={styles.slider} key={i}>
+              <div className={styles.slider} key={i} onDoubleClick={() => handleDoubleClick(d.target)}>
                 <label className={styles.label}>{d.target}</label>
                 <span className={styles.track}></span>
               </div>
