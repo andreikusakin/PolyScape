1179f04482df5c2502632c05b37bdc38efa75d2f
andreikusakin <andrewkusakin@me.com>
2024-02-14 20:48:19 -0500

    Update Slider component styles and functionality

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import { LFOTarget } from "@/lib/types/types";
 import styles from "./Slider.module.css";
+import { useDrag } from "@use-gesture/react";
 
 type SliderProps = {
   key: number;
   lfoName: string;
-  target: string;
+  target: LFOTarget;
   value: number;
   min: number;
   max: number;
+  step: number;
+  onChange: (target: LFOTarget, value: number) => void;
   handleDoubleClick: (target: LFOTarget) => void;
 };
 
@@ -18,10 +23,51 @@ export const Slider = ({
   value,
   min,
   max,
+  step,
   handleDoubleClick,
+  onChange
 }: SliderProps) => {
+
+  const adjustValueToStep = (value: number, step: number) => {
+    const roundedSteps = Math.round((value - min) / step);
+    return roundedSteps * step + min;
+  };
+  const updateValue = (value: number) => {
+    let adjustedValue = adjustValueToStep(value, step);
+    return Math.min(Math.max(adjustedValue, min), max);
+  };
+
+  const valueToPercent = (value: number) => {
+    return ((value - min) / (max - min)) * 100;
+  };
+
+  const percentToValue = (percent: number) => {
+    return (percent / 100) * (max - min) + min;
+  };
+
+  const bind = useDrag(
+    ({ delta }) => {
+      const dragSensitivity = -0.3;
+      let percentChange = delta[1] * dragSensitivity;
+      let currentPercent = valueToPercent(value);
+      let newPercent = Math.max(
+        0,
+        Math.min(100, currentPercent + percentChange)
+      );
+      let newValue = percentToValue(newPercent);
+
+      newValue = updateValue(newValue);
+      onChange(target, newValue);
+    },
+    {
+      pointer: {
+        keys: false,
+      },
+    }
+  );
+
   return (
-    <div
+    <div {...bind()}
       className={[
         styles.container,
         lfoName === "lfo1" ? styles.containerLFO1 : styles.containerLFO2,
@@ -42,6 +88,7 @@ export const Slider = ({
           styles.track,
           lfoName === "lfo1" ? styles.trackLFO1 : styles.trackLFO2,
         ].join(" ")}
+        style={{width: `${value * 100}%`}}
       ></span>
     </div>
   );
