bdfe45fc5c4b6a724203938faeb4ad74c74d832c
andreikusakin <andrewkusakin@me.com>
2024-02-16 01:16:16 -0500

    Removed wide prop

--- src/components/SectionWrapper/SectionWrapper.tsx
+++ src/components/SectionWrapper/SectionWrapper.tsx
@@ -4,22 +4,13 @@ import styles from "./SectionWrapper.module.css";
 type SectionWrapperProps = {
   name: string;
   children: React.ReactNode;
-  wide?: boolean;
 };
 
-export const SectionWrapper = ({
-  name,
-  children,
-  wide,
-}: SectionWrapperProps) => {
+export const SectionWrapper = ({ name, children }: SectionWrapperProps) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.name}>{name}</div>
-      <div
-        className={[wide ? styles.container_wide : styles.container].join(" ")}
-      >
-        {children}
-      </div>
+      <div className={styles.container}>{children}</div>
     </div>
   );
 };
