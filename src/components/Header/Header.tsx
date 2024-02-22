3d034b36bc7e8e89a2e02cb3b273506daa264934
andreikusakin <andrewkusakin@me.com>
2024-02-21 20:18:51 -0500

    Added some animations using framer motion

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,116 @@
+import CustomPolySynth from "@/lib/engines/CustomPolySynth";
 import { SectionWrapper } from "../SectionWrapper/SectionWrapper";
 import styles from "./Header.module.css";
 import { WebMidi } from "webmidi";
+import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
-export const Header = () => {
-  WebMidi.enable();
-  const midiInput = WebMidi.inputs;
-  console.log(midiInput);
+type HeaderProps = {
+  engine: CustomPolySynth;
+  isUiVisible: boolean;
+  setIsUiVisible: (isUiVisible: boolean) => void;
+};
+
+export const Header = ({
+  engine,
+  isUiVisible,
+  setIsUiVisible,
+}: HeaderProps) => {
+
+  const [midiInputs, setMidiInputs] = useState<typeof WebMidi.inputs>();
+  const [isMidiSupported, setIsMidiSupported] = useState<boolean>(false);
+  const [isSelectingMidi, setIsSelectingMidi] = useState<boolean>(false);
+  useEffect(() => {
+    if (WebMidi.enabled) {
+      setMidiInputs(WebMidi.inputs);
+
+      console.log(WebMidi.enabled);
+    }
+    setIsMidiSupported(engine?.isMidiSupported);
+  }, [engine]);
+
+  const hideUiAnimation = {
+    width: isUiVisible ? "100%" : "fit-content",
+  };
+
   return (
-    <SectionWrapper>
-      <div className={styles.container}>
-        <div className={styles.midi}>
-          {midiInput.map((input, i) => (
-            <div key={i}>{input.name}</div>
-          ))}
-        </div>
-        <div className={styles.bpm}>bpm</div>
-        <div className={styles.fx}>fx</div>
-      </div>
-    </SectionWrapper>
+    <motion.div
+      className={styles.wrapper}
+      animate={hideUiAnimation}
+      transition={{ duration: 0.5 }}
+    >
+      <SectionWrapper>
+        <AnimatePresence>
+          {isUiVisible && (
+            <motion.div
+              className={styles.container}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+              exit={{ opacity: 0 }}
+            >
+              <div className={styles.midi}>
+                <label>Midi</label>
+                {!isMidiSupported && !midiInputs && (
+                  <div
+                    className={[
+                      styles.midi_selector,
+                      styles.no_midi_support,
+                    ].join(" ")}
+                  >
+                    No Midi Support in Your Browser
+                  </div>
+                )}
+                {midiInputs && (
+                  <div
+                    className={styles.midi_selector}
+                    onClick={() => setIsSelectingMidi(!isSelectingMidi)}
+                  >
+                    <label>{midiInputs[engine?.midiInputIndex].name}</label>
+                    {isSelectingMidi && (
+                      <div className={styles.selecting_midi}>
+                        {midiInputs.map((input, i) => (
+                          <div
+                            key={i}
+                            onClick={() => engine?.setMidiInputByIndex(i)}
+                          >
+                            {input.name}
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                )}
+              </div>
+              <div className={styles.preset}>preset</div>
+              <div className={styles.bpm}>bpm</div>
+
+              <div className={styles.buttons}>
+                <button className={styles.button}>fx</button>
+
+                <button
+                  className={styles.button}
+                  onClick={() => setIsUiVisible(!isUiVisible)}
+                >
+                  hide ui
+                </button>
+              </div>
+            </motion.div>
+          )}
+          {!isUiVisible && (
+            <motion.button
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.5, duration: 0.5 }}
+              exit={{ opacity: 0 }}
+              className={styles.show_ui_button}
+              onClick={() => setIsUiVisible(!isUiVisible)}
+            >
+              show ui
+            </motion.button>
+          )}
+        </AnimatePresence>
+      </SectionWrapper>
+    </motion.div>
   );
 };
