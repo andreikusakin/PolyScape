d1cd2e93b47dff88a914543a151954a90eb20233
andreikusakin <andrewkusakin@me.com>
2024-02-25 21:00:32 -0500

    Added fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,31 @@
-import type { Metadata } from 'next'
-import { Inter, Exo } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter, Exo } from "next/font/google";
+import "./globals.css";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-const exo = Exo({ subsets: ['latin'] })
+const exo = Exo({ subsets: ["latin"] });
+
+
 
 export const metadata: Metadata = {
-  title: 'Synthesizer',
-  description: 'Synthesizer',
-  keywords: [
-    "Synthesizer",
-  ],
-  creator: 'Andrew Kusakin',
+  title: "Synthesizer",
+  description: "Synthesizer",
+  keywords: ["Synthesizer"],
+  creator: "Andrew Kusakin",
 
-  category: 'Music',
-}
+  category: "Music",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={exo.className}>{children}</body>
+      <body className={`${exo.className}`}>{children}</body>
     </html>
-  )
+  );
 }
