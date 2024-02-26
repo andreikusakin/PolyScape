d499c7e2caef24d4e5d61b5b25f77d61c06d67e8
andreikusakin <andrewkusakin@me.com>
2024-02-25 21:10:05 -0500

    Added PingPongDelay Rackets and Reverb Cube components

---
+++ src/components/Reverb/Cube/Cube.tsx
@@ -0,0 +1,23 @@
+import styles from "./Cube.module.css";
+import localFont from "next/font/local";
+
+const kodeMono = localFont({ src: "./../KodeMono-VariableFont_wght.ttf" });
+
+export const Cube = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.cube}>
+        <div className={`${styles.face} ${styles.top}`}></div>
+        <div className={`${styles.text} ${kodeMono.className}`}>
+          <span>rev</span>
+          <span>erb</span>
+        </div>
+        <div className={`${styles.face} ${styles.bottom}`}></div>
+        <div className={`${styles.face} ${styles.left}`}></div>
+        <div className={`${styles.face} ${styles.right}`}></div>
+        <div className={`${styles.face} ${styles.front}`}></div>
+        <div className={`${styles.face} ${styles.back}`}></div>
+      </div>
+    </div>
+  );
+};
