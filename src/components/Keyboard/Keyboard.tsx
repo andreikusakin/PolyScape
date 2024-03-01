7bc46ca398407d79f52371898d590048ce62e14a
andreikusakin <andrewkusakin@me.com>
2024-03-01 01:49:01 -0500

    Refactor Keyboard component to remove unused props

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -6,17 +6,13 @@ import { useState } from "react";
 
 type KeyboardProps = {
   engine: CustomPolySynth;
-  osc1Type: string;
-  osc2Type: string;
 };
 
-export const Keyboard = ({ engine, osc1Type, osc2Type }: KeyboardProps) => {
-  const [color, setColor] = useState<string>("255, 255, 0");
-
+export const Keyboard = ({ engine }: KeyboardProps) => {
   return (
     <SectionWrapper>
       <div className={styles.container}>
-        <Keys osc1Type={osc1Type} osc2Type={osc2Type} engine={engine} />
+        <Keys engine={engine} />
       </div>
     </SectionWrapper>
   );
