57200c3b5f57741ff101cb5a3d8c8f31510d540c
andreikusakin <andrewkusakin@me.com>
2024-03-01 01:50:58 -0500

    Refactor Oscillator component to use settingsStore

--- src/components/Oscillator/Oscillator.tsx
+++ src/components/Oscillator/Oscillator.tsx
@@ -1,33 +1,38 @@
-import React from "react";
+import React, { use } from "react";
 import Knob from "../Knob/Knob";
 import styles from "./Oscillator.module.css";
 import { SectionWrapper } from "../SectionWrapper/SectionWrapper";
 import CustomPolySynth from "@/lib/engines/CustomPolySynth";
 import { LFOTarget, Preset } from "@/lib/types/types";
 import { OscillatorWaveform } from "../OscillatorWaveform";
+import { useSynthSettingsStore } from "@/lib/store/settingsStore";
 
 type OscillatorProps = {
   engine: CustomPolySynth | undefined;
-  name: string;
-  settings: Preset["osc1"] | Preset["osc2"];
-  updateSettings: (settings: Preset["osc1"] | Preset["osc2"]) => void;
+  oscNumber: number;
   isSelectingLFO: false | 1 | 2;
   assignLFO: (target: LFOTarget, lfo: 1 | 2) => void;
 };
 
 const Oscillator: React.FC<OscillatorProps> = ({
   engine,
-  name,
-  settings,
-  updateSettings,
+  oscNumber,
   isSelectingLFO,
   assignLFO,
 }) => {
+  const { settings, updateSettings } = useSynthSettingsStore((state) => (
+    {
+      settings: oscNumber === 1 ? state.osc1 : state.osc2,
+      updateSettings: oscNumber === 1 ? state.setOsc1Params : state.setOsc2Params,
+    }
+  )
+    
+  );
   const updateWaveformType = (
     type: "sine" | "sawtooth" | "pulse" | "triangle"
   ) => {
     updateSettings({ ...settings, type: type });
-    if (name === "osc1") {
+    if (oscNumber === 1) {
       engine?.voices.forEach((v) => (v.oscillator.type = type));
       engine?.disconnectLFO("osc1 pulse width", 1);
     } else {
@@ -35,7 +40,7 @@ const Oscillator: React.FC<OscillatorProps> = ({
     }
 
     if (type === "pulse") {
-      name === "osc1"
+      oscNumber === 1
         ? engine?.voices.forEach((v) => (v.oscillator.width.value = settings.pulseWidth))
         : engine?.voices.forEach(
             (v) => (v.oscillator2.width.value = settings.pulseWidth)
@@ -45,7 +50,7 @@ const Oscillator: React.FC<OscillatorProps> = ({
 
   const updatePulseWidth = (value: number) => {
     updateSettings({ ...settings, pulseWidth: value });
-    name === "osc1"
+    oscNumber === 1
       ? engine?.voices.forEach((v) => (v.oscillator.width.value = value))
       : engine?.voices.forEach((v) => (v.oscillator2.width.value = value));
   };
@@ -54,7 +59,7 @@ const Oscillator: React.FC<OscillatorProps> = ({
     updateSettings({ ...settings, transpose: value });
     const coarseValue = value * 100 + settings.detune;
 
-    name === "osc1"
+    oscNumber === 1
       ? engine?.voices.forEach((v) => v.oscillator.set({ detune: coarseValue }))
       : engine?.voices.forEach(
           (v) => (v.oscillator2.detune.value = coarseValue)
@@ -64,7 +69,7 @@ const Oscillator: React.FC<OscillatorProps> = ({
   const updateFine = (value: number) => {
     updateSettings({ ...settings, detune: value });
     const fineValue = value + settings.transpose * 100;
-    name === "osc1"
+    oscNumber === 1
       ? engine?.voices.forEach((v) => (v.oscillator.detune.value = fineValue))
       : engine?.voices.forEach((v) => (v.oscillator2.detune.value = fineValue));
   };
@@ -87,12 +92,12 @@ const Oscillator: React.FC<OscillatorProps> = ({
       min: -70 + value,
       max: 12 + value,
     });
-    name === "osc1"
+    oscNumber === 1
       ? engine?.voices.forEach((v) => (v.oscillator.volume.value = value))
       : engine?.voices.forEach((v) => (v.oscillator2.volume.value = value));
   };
   return (
-    <SectionWrapper name={name}>
+    <SectionWrapper name={`osc${oscNumber}`}>
       <div className={styles.grid}>
         <ul className={styles.shapes}>
           <li
@@ -130,7 +135,7 @@ const Oscillator: React.FC<OscillatorProps> = ({
           exponent={1}
           label={"pulse width"}
           lfoParameter={
-            name === "osc1" ? "osc1 pulse width" : "osc2 pulse width"
+            oscNumber === 1 ? "osc1 pulse width" : "osc2 pulse width"
           }
           radius={24}
           minValue={-1}
@@ -148,7 +153,7 @@ const Oscillator: React.FC<OscillatorProps> = ({
         <Knob
           exponent={1}
           label={"coarse"}
-          lfoParameter={name === "osc1" ? "osc1 coarse" : "osc2 coarse"}
+          lfoParameter={oscNumber === 1 ? "osc1 coarse" : "osc2 coarse"}
           minValue={-24}
           maxValue={24}
           currentValue={settings.transpose}
@@ -164,7 +169,7 @@ const Oscillator: React.FC<OscillatorProps> = ({
         <Knob
           exponent={1}
           label={"fine"}
-          lfoParameter={name === "osc1" ? "osc1 fine" : "osc2 fine"}
+          lfoParameter={oscNumber === 1 ? "osc1 fine" : "osc2 fine"}
           minValue={-100}
           maxValue={100}
           currentValue={settings.detune}
@@ -179,7 +184,7 @@ const Oscillator: React.FC<OscillatorProps> = ({
         <Knob
           exponent={1}
           label={"volume"}
-          lfoParameter={name === "osc1" ? "osc1 volume" : "osc2 volume"}
+          lfoParameter={oscNumber === 1 ? "osc1 volume" : "osc2 volume"}
           minValue={-70}
           maxValue={12}
           unit={"db"}
