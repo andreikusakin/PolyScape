b6711a59c5c068b5161d94eba78015f57a706f55
andreikusakin <andrewkusakin@me.com>
2024-03-01 01:49:50 -0500

    Refactor settingsStore.ts and add useWaveformColor store

--- src/lib/store/settingsStore.ts
+++ src/lib/store/settingsStore.ts
@@ -1,13 +1,14 @@
 import { initPreset } from "../presets/Init";
 import { create } from "zustand";
 import { Preset } from "../types/types";
+import { colorMap } from "../utils/colorMap";
 
 export const useSynthSettingsStore = create((set) => ({
   presetName: initPreset.presetName,
   osc1: initPreset.osc1,
   osc2: initPreset.osc2,
   noise: initPreset.noise,
-    envelopeAmplitude: initPreset.envelopeAmplitude,
+  envelopeAmplitude: initPreset.envelopeAmplitude,
   isSelectingLFO: false,
   setAllParamsFromPreset: (preset: Preset) => {
     set({
@@ -17,10 +18,25 @@ export const useSynthSettingsStore = create((set) => ({
       noise: preset.noise,
     });
   },
+  setOsc1Params: (param) => {
+    set((state) => ({ osc1: { ...state.osc1, ...param } }));
+  },
+  setOsc2Params: (param) => {
+    set((state) => ({ osc2: { ...state.osc2, ...param } }));
+  },
+
   setNoiseParams: (param) => {
     set((state) => ({ noise: { ...state.noise, ...param } }));
   },
   setEnvAmplitudeParams: (param) => {
-    set((state) => ({ envelopeAmplitude: { ...state.envelopeAmplitude, ...param } }));
-  }
+    set((state) => ({
+      envelopeAmplitude: { ...state.envelopeAmplitude, ...param },
+    }));
+  },
 }));
+
+export const useWaveformColor = () =>
+  useSynthSettingsStore((state) => {
+    const key = `${state.osc1.type}-${state.osc2.type}`;
+    return colorMap[key];
+  });
