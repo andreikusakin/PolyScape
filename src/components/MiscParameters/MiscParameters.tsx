fe627480a19a5fea336c46893ba1094f006cc87e
andreikusakin <andrewkusakin@me.com>
2024-03-03 21:12:30 -0500

    Add styles for unison and hold buttons

--- src/components/MiscParameters/MiscParameters.tsx
+++ src/components/MiscParameters/MiscParameters.tsx
@@ -3,29 +3,33 @@
 import {
   useSynthEngineStore,
   useSynthSettingsStore,
+  useWaveformColor,
 } from "@/lib/store/settingsStore";
 import Knob from "../Knob/Knob";
 import { SectionWrapper } from "../SectionWrapper/SectionWrapper";
 import styles from "./MiscParameters.module.css";
 import shallow from "zustand/shallow";
-import { useShallow } from 'zustand/react/shallow'
+import { useShallow } from "zustand/react/shallow";
 
 // PanSpread
 // Unison
-// Vintage knob
+// HOLD
 
 export const MiscParameters = () => {
-  console.log("RERENDER MISC PARAMETERS")
+  console.log("RERENDER MISC PARAMETERS");
   const engine = useSynthEngineStore((state) => state.synthEngine);
+  const colorRGB = useWaveformColor();
   const { panSpread, unison, setPanSpread, setUnison, detune, setDetune } =
-    useSynthSettingsStore(useShallow((state) => ({
-      panSpread: state.panSpread,
-      unison: state.unison,
-      setPanSpread: state.setPanSpread,
-      setUnison: state.setUnison,
-      detune: state.detune,
-      setDetune: state.setDetune,
-    })));
+    useSynthSettingsStore(
+      useShallow((state) => ({
+        panSpread: state.panSpread,
+        unison: state.unison,
+        setPanSpread: state.setPanSpread,
+        setUnison: state.setUnison,
+        detune: state.detune,
+        setDetune: state.setDetune,
+      }))
+    );
   const updatePanSpread = (value: number) => {
     engine.setPanSpread(value);
     setPanSpread(value);
@@ -38,26 +42,18 @@ export const MiscParameters = () => {
   const handleUnison = () => {
     setUnison(!unison);
     engine.unison = !unison;
+  };
 
-  }
-
-    const updateDetune = (value: number) => {
-        engine.voices.forEach((v, i) => {
-            const currentDetuneOsc1 = v.oscillator.detune.value;
-            const currentDetuneOsc2 = v.oscillator2.detune.value;
-            const coefficientOsc1 = i % 2 === 0 ? -1 : 1;
-            const coefficientOsc2 = i % 2 === 0 ? 1 : -1;
-            const randomValue = Math.random() * (value - value / 4 + 1) + value / 4;
-            v.oscillator.detune.value = v.oscillator.detune.value - (currentDetuneOsc1 + coefficientOsc1) + (randomValue * coefficientOsc1);
-            v.oscillator2.detune.value = v.oscillator2.detune.value - (currentDetuneOsc2 + coefficientOsc2) + (randomValue * coefficientOsc2);
-            
-        }
-            )
-            setDetune(value);
-    };
+  const updateDetune = (value: number) => {
+    engine.setDetune(value);
+    setDetune(value);
+  };
   return (
     <SectionWrapper name="Misc">
-      <div className={styles.grid}>
+      <div
+        className={styles.grid}
+        style={{ "--color-rgb": colorRGB } as React.CSSProperties}
+      >
         <Knob
           exponent={1}
           label="Pan Spread"
@@ -82,7 +78,17 @@ export const MiscParameters = () => {
           interactive
           onChange={updateDetune}
         />
-        <div className={styles.unison} onClick={handleUnison}>unison</div>
+        <div className={styles.right}>
+          <div
+            className={[styles.button, unison ? styles.button_active : ""].join(
+              " "
+            )}
+            onClick={handleUnison}
+          >
+            unison
+          </div>
+          <div className={styles.button}>hold</div>
+        </div>
       </div>
     </SectionWrapper>
   );
