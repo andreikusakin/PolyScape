29634c480d2a4ed7f7161f5cf5b51d1ea12f8d17
andreikusakin <andrewkusakin@me.com>
2024-03-17 19:39:30 -0400

    Add wrapper

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -4,13 +4,14 @@ import { SectionWrapper } from "../SectionWrapper/SectionWrapper";
 import { Keys } from "./Keys/Keys";
 import { useState } from "react";
 
-
 export const Keyboard = () => {
   return (
-    <SectionWrapper>
-      <div className={styles.container}>
-        <Keys />
-      </div>
-    </SectionWrapper>
+    <div className={styles.wrapper}>
+      <SectionWrapper>
+        <div className={styles.container}>
+          <Keys />
+        </div>
+      </SectionWrapper>
+    </div>
   );
 };
