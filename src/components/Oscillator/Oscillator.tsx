da123ba2f89e1016183d7ed5d660b9a57f56a900
andreikusakin <andrewkusakin@me.com>
2024-03-21 01:23:33 -0400

    Refactor

--- src/components/Oscillator/Oscillator.tsx
+++ src/components/Oscillator/Oscillator.tsx
@@ -16,12 +16,15 @@ type OscillatorProps = {
 };
 
 const Oscillator = ({ oscNumber }: OscillatorProps) => {
-  console.log("RERENDER OSCILLATOR")
+  console.log("RERENDER OSCILLATOR");
   const engine = useSynthEngineStore((state) => state.synthEngine);
-  const { settings, updateSettings } = useSynthSettingsStore(useShallow((state) => ({
-    settings: oscNumber === 1 ? state.osc1 : state.osc2,
-    updateSettings: oscNumber === 1 ? state.setOsc1Params : state.setOsc2Params,
-  })));
+  const { settings, updateSettings } = useSynthSettingsStore(
+    useShallow((state) => ({
+      settings: oscNumber === 1 ? state.osc1 : state.osc2,
+      updateSettings:
+        oscNumber === 1 ? state.setOsc1Params : state.setOsc2Params,
+    }))
+  );
   const updateWaveformType = (
     type: "sine" | "sawtooth" | "pulse" | "triangle"
   ) => {
@@ -54,7 +57,6 @@ const Oscillator = ({ oscNumber }: OscillatorProps) => {
   const updateCoarse = (value: number) => {
     updateSettings({ transpose: value });
     engine.setCoarse(value, oscNumber);
-  
   };
 
   const updateFine = (value: number) => {
@@ -86,98 +88,100 @@ const Oscillator = ({ oscNumber }: OscillatorProps) => {
   };
   return (
     <SectionWrapper name={`osc${oscNumber}`}>
-      <div className={styles.grid}>
-        <ul className={styles.shapes}>
-          <li
-            onClick={() => updateWaveformType("sine")}
-            className={settings.type === "sine" ? styles.selected : ""}
-          >
-            sine
-          </li>
-          <li
-            onClick={() => updateWaveformType("sawtooth")}
-            className={settings.type === "sawtooth" ? styles.selected : ""}
-          >
-            sawtooth
-          </li>
-          <li
-            onClick={() => updateWaveformType("pulse")}
-            className={settings.type === "pulse" ? styles.selected : ""}
-          >
-            square
-          </li>
-          <li
-            onClick={() => updateWaveformType("triangle")}
-            className={settings.type === "triangle" ? styles.selected : ""}
-          >
-            triangle
-          </li>
-        </ul>
+      <div className={styles.container}>
+        <div className={styles.grid}>
+          <ul className={styles.shapes}>
+            <li
+              onClick={() => updateWaveformType("sine")}
+              className={settings.type === "sine" ? styles.selected : ""}
+            >
+              sine
+            </li>
+            <li
+              onClick={() => updateWaveformType("sawtooth")}
+              className={settings.type === "sawtooth" ? styles.selected : ""}
+            >
+              sawtooth
+            </li>
+            <li
+              onClick={() => updateWaveformType("pulse")}
+              className={settings.type === "pulse" ? styles.selected : ""}
+            >
+              square
+            </li>
+            <li
+              onClick={() => updateWaveformType("triangle")}
+              className={settings.type === "triangle" ? styles.selected : ""}
+            >
+              triangle
+            </li>
+          </ul>
 
-        <div className={styles.waveform}>
-          <div className={styles.waveformAnimation}>
-            <OscillatorWaveform oscType={settings.type} />
+          <div className={styles.waveform}>
+            <div className={styles.waveformAnimation}>
+              <OscillatorWaveform oscType={settings.type} />
+            </div>
           </div>
+          <Knob
+            exponent={1}
+            label={"pulse width"}
+            lfoParameter={
+              oscNumber === 1 ? "osc1 pulse width" : "osc2 pulse width"
+            }
+            radius={24}
+            minValue={-1}
+            maxValue={1}
+            currentValue={settings.pulseWidth}
+            step={0.01}
+            onChange={updatePulseWidth}
+            // lfoPercent={100}
+            startingPoint={"middle"}
+            interactive={settings.type === "pulse" ? true : false}
+          />
+
+          <Knob
+            exponent={1}
+            label={"coarse"}
+            lfoParameter={oscNumber === 1 ? "osc1 coarse" : "osc2 coarse"}
+            minValue={-24}
+            maxValue={24}
+            currentValue={settings.transpose}
+            step={1}
+            onChange={updateCoarse}
+            radius={24}
+            startingPoint={"middle"}
+            interactive={true}
+
+            // lfoAmount={lfo1?.find((lfo) => lfo.target === "osc1 coarse")?.amount}
+          />
+          <Knob
+            exponent={1}
+            label={"fine"}
+            lfoParameter={oscNumber === 1 ? "osc1 fine" : "osc2 fine"}
+            minValue={-100}
+            maxValue={100}
+            currentValue={settings.detune}
+            step={1}
+            onChange={updateFine}
+            radius={24}
+            startingPoint={"beginning"}
+            interactive={true}
+          />
+          <Knob
+            exponent={1}
+            label={"volume"}
+            lfoParameter={oscNumber === 1 ? "osc1 volume" : "osc2 volume"}
+            minValue={-70}
+            maxValue={12}
+            unit={"db"}
+            currentValue={settings.volume}
+            step={0.5}
+            onChange={updateVolume}
+            radius={24}
+            startingPoint={"middle"}
+            interactive={true}
+          />
         </div>
-        <Knob
-          exponent={1}
-          label={"pulse width"}
-          lfoParameter={
-            oscNumber === 1 ? "osc1 pulse width" : "osc2 pulse width"
-          }
-          radius={24}
-          minValue={-1}
-          maxValue={1}
-          currentValue={settings.pulseWidth}
-          step={0.01}
-          onChange={updatePulseWidth}
-          // lfoPercent={100}
-          startingPoint={"middle"}
-          interactive={settings.type === "pulse" ? true : false}
-        />
-
-        <Knob
-          exponent={1}
-          label={"coarse"}
-          lfoParameter={oscNumber === 1 ? "osc1 coarse" : "osc2 coarse"}
-          minValue={-24}
-          maxValue={24}
-          currentValue={settings.transpose}
-          step={1}
-          onChange={updateCoarse}
-          radius={24}
-          startingPoint={"middle"}
-          interactive={true}
-
-          // lfoAmount={lfo1?.find((lfo) => lfo.target === "osc1 coarse")?.amount}
-        />
-        <Knob
-          exponent={1}
-          label={"fine"}
-          lfoParameter={oscNumber === 1 ? "osc1 fine" : "osc2 fine"}
-          minValue={-100}
-          maxValue={100}
-          currentValue={settings.detune}
-          step={1}
-          onChange={updateFine}
-          radius={24}
-          startingPoint={"beginning"}
-          interactive={true}
-        />
-        <Knob
-          exponent={1}
-          label={"volume"}
-          lfoParameter={oscNumber === 1 ? "osc1 volume" : "osc2 volume"}
-          minValue={-70}
-          maxValue={12}
-          unit={"db"}
-          currentValue={settings.volume}
-          step={0.5}
-          onChange={updateVolume}
-          radius={24}
-          startingPoint={"middle"}
-          interactive={true}
-        />
       </div>
     </SectionWrapper>
   );
