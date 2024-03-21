877501012760864daa48af3987979329ab71843e
andreikusakin <andrewkusakin@me.com>
2024-03-21 01:23:24 -0400

    Add ChorusImage component with CSS styling

--- src/components/Chorus/ChorusImage/ChorusImage.tsx
+++ src/components/Chorus/ChorusImage/ChorusImage.tsx
@@ -1,7 +1,12 @@
 import styles from "./ChorusImage.module.css";
+import { Bungee_Outline } from "next/font/google";
+
+const bungee = Bungee_Outline({ weight: "400", subsets: ["latin"] });
 
 export const ChorusImage = () => {
   return (
-    <div>ChorusImage</div>
+    <div className={`${bungee.className} ${styles.container}`}>
+      <h3>Chorus</h3>
+      </div>
   )
 }
