746cbec7b23aa5380d931475c5a1c8f9d1f779c5
andreikusakin <andrewkusakin@me.com>
2024-04-23 00:09:07 -0400

    Refactor presets route to include POST method for creating new presets

--- src/app/api/presets/route.ts
+++ src/app/api/presets/route.ts
@@ -1,13 +1,48 @@
 import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   const presets = await prisma.preset.findMany({
     select: {
-      presetName: true,
-      discription: true,
+      id: true,
+      name: true,
+      type: true,
+      description: true,
       author: true,
-      presetData: true,
+      settings: true,
+      default: true,
     },
+    orderBy: {
+      default: 'desc' 
+    }
   });
-  return Response.json(presets);
+  return new NextResponse(JSON.stringify(presets), {
+    headers: { 'Content-Type': 'application/json' },
+    status: 200,
+  });
 }
+
+export async function POST(request) {
+    try {
+      const data = await request.json();
+      const newPreset = await prisma.preset.create({
+        data: {
+          name: data.name,
+          type: data.type,
+          email: data.email,
+          description: data.description,
+          author: data.author,
+          settings: data.settings,
+        },
+      });
+      return new NextResponse(JSON.stringify({ message: "Successfully saved preset!"}), {
+        headers: { 'Content-Type': 'application/json' },
+        status: 201,
+      });
+    } catch (error) {
+      return new NextResponse(JSON.stringify({ error: error.message }), {
+        headers: { 'Content-Type': 'application/json' },
+        status: 500,
+      });
+    }
+  }
