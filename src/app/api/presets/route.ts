df30a2347100741567f545208b5081708bc84e39
andreikusakin <andrewkusakin@me.com>
2024-04-23 16:10:14 -0400

    Add delete

--- src/app/api/presets/route.ts
+++ src/app/api/presets/route.ts
@@ -46,3 +46,49 @@ export async function POST(request) {
       });
     }
   }
+
+  export async function DELETE(request) {
+    try {
+      const data = await request.json();
+      const preset = await prisma.preset.findUnique({
+        where: {
+          id: data.id,
+          name: data.name,
+        },
+        select: {
+          email: true 
+        }
+      });
+  
+      if (!preset) {
+        return new NextResponse(JSON.stringify({ error: "Preset not found." }), {
+          headers: { 'Content-Type': 'application/json' },
+          status: 404,
+        });
+      }
+  
+      if (preset.email !== data.email) {
+        return new NextResponse(JSON.stringify({ error: "Unauthorized to delete this preset." }), {
+          headers: { 'Content-Type': 'application/json' },
+          status: 403,
+        });
+      }
+  
+      const deletedPreset = await prisma.preset.delete({
+        where: {
+          id: data.id,
+        },
+      });
+  
+      return new NextResponse(JSON.stringify({ message: "Successfully deleted preset!" }), {
+        headers: { 'Content-Type': 'application/json' },
+        status: 200,
+      });
+    } catch (error) {
+      return new NextResponse(JSON.stringify({ error: error.message }), {
+        headers: { 'Content-Type': 'application/json' },
+        status: 500,
+      });
+    }
+  }
+  
