a58a9a1302af6ab185a304b0a3c028f69bf22661
andreikusakin <andrewkusakin@me.com>
2024-04-24 13:25:35 -0400

    types

--- src/lib/store/presetLibraryStore.ts
+++ src/lib/store/presetLibraryStore.ts
@@ -2,17 +2,19 @@ import { create } from "zustand";
 import { Preset } from "../types/types";
 import { presetsList } from "../presets/presetsList";
 
+type LibraryPreset = {
+  id: string;
+  name: string;
+  type: string;
+  description: string;
+  author: string;
+  settings: Preset;
+};
+
 type PresetLibraryStore = {
   currentPreset: Preset | null;
-  presetLibrary: {
-    id: string;
-    name: string;
-    type: string;
-    description: string;
-    author: string;
-    settings: Preset;
-  } | null;
-  selectedPreset: String;
+  presetLibrary: LibraryPreset[];
+  selectedPreset: string;
   setCurrentPreset: (preset: Preset) => void;
   setSelectedPreset: (id: String) => void;
   setPresetLibrary: (presets: {
@@ -23,18 +25,23 @@ type PresetLibraryStore = {
     author: string;
     settings: Preset;
   }) => void;
-  addPreset: (preset: Preset) => void;
+  addPreset: (preset: LibraryPreset) => void;
+  deletePresetFromLibrary: (id: string) => void;
 };
 
 export const usePresetLibraryStore = create<PresetLibraryStore>((set) => ({
   currentPreset: null,
-  presetLibrary: null,
-  selectedPreset: '',
+  presetLibrary: [],
+  selectedPreset: "",
   setCurrentPreset: (preset) => set({ currentPreset: preset }),
   setSelectedPreset: (selectedPreset) => set({ selectedPreset }),
   setPresetLibrary: (presets) => set({ presetLibrary: presets }),
   addPreset: (preset) =>
     set((state) => ({
       presetLibrary: [...state.presetLibrary, preset],
+    })),
+  deletePresetFromLibrary: (id) =>
+    set((state) => ({
+      presetLibrary: state.presetLibrary.filter((p) => p.id !== id),
     })),
 }));
