b025fa5239cb667a4562fcffa0c67a0320c79929
andreikusakin <andrewkusakin@me.com>
2024-04-27 20:11:20 -0400

    Delete

--- Prisma/migrations/20240419032518_typo/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `discription` on the `Preset` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Preset" DROP COLUMN "discription",
-ADD COLUMN     "description" TEXT;
