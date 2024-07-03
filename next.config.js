a33b417f37559b928c4a9ba27efc9f63b5bddee0
andreikusakin <andrewkusakin@me.com>
2024-07-02 21:21:24 -0400

    glsl config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: false };
+const nextConfig = {
+  reactStrictMode: false,
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs)$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'raw-loader',
+        },
+        {
+          loader: 'glslify-loader',
+        },
+      ],
+    });
+    return config;
+  },
+};
 
 module.exports = nextConfig;
