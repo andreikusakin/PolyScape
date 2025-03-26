8a7581de838a4081b1d4e0fe7f859da91728ae17
andreikusakin <andrewkusakin@me.com>
2025-03-26 17:31:11 -0400

    added welcome component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+"use client";
+import { Welcome } from "@/components/Welcome/Welcome";
+import { useAudioContextStore } from "@/lib/store/audioContextStore";
 import { useEffectsEngineStore } from "@/lib/store/settingsStore";
 import dynamic from "next/dynamic";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 
 const Visualization = dynamic(() => import("@/components/Visualization"), {
   ssr: false,
@@ -10,13 +13,23 @@ const PolySynth = dynamic(() => import("@/components/PolySynth/PolySynth"), {
   ssr: false,
 });
 
-export default async function Home() {
+
+
+export default function Home() {
+  const { contextStarted } = useAudioContextStore(
+    (state) => ({
+      contextStarted: state.contextStarted,
+  
+    })
+  );
 
   return (
     <main>
       <Suspense fallback={<div>Loading...</div>}>
+        <Welcome />
         {/* <Visualization /> */}
-        <PolySynth />
+        {contextStarted && <PolySynth />}
+        
       </Suspense>
     </main>
   );
---
+++ src/components/Welcome/Welcome.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { useAudioContextStore } from "@/lib/store/audioContextStore";
+import React from "react";
+import * as Tone from "tone/build/esm/index";
+
+export const Welcome = () => {
+  const { contextStarted, setContextStarted } = useAudioContextStore(
+    (state) => ({
+      contextStarted: state.contextStarted,
+      setContextStarted: state.setContextStarted,
+    })
+  );
+  const [ displayWelcome, setDisplayWelcome ] = React.useState(true);
+  const handleStart = async () => {
+    await Tone.start();
+    if (Tone.getContext().state == "running") {
+      setContextStarted(true);
+      console.log("Audio context started");
+      setDisplayWelcome(false);
+    }
+  };
+
+  return (
+    <div style={{
+      display: displayWelcome ? "flex" : "none",
+      alignItems: "center",
+      justifyContent: "center",
+    }}>
+      <div><p>
+        Introducing our cutting-edge 10-voice synthesizer that brings
+        studio-quality sound creation right to your desktop. With dual
+        oscillators per voice, you can layer rich timbres and create complex
+        sounds. A versatile filter section—complete with its own dedicated
+        envelope—lets you sculpt and shape your sound with precision. Two
+        low-frequency oscillators (LFOs) add dynamic modulation options, perfect
+        for evolving textures and rhythmic effects. MIDI support ensures
+        seamless integration with your hardware, while the built-in virtual
+        keyboard makes it easy to experiment even without external gear. Plus,
+        an innovative AI-powered preset generator helps you discover new sounds
+        by creating unique preset ideas tailored to your creative vision.
+        Whether you're a seasoned sound designer or just starting out, this app
+        offers a powerful and intuitive platform for exploring synthesis and
+        pushing the boundaries of your music.
+      </p>
+      <button onClick={handleStart}>Start</button></div>
+      
+    </div>
+  );
+};
