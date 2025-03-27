068b4c3813aec89ef5cf84c99c5050d5b267e82a
andreikusakin <andrewkusakin@me.com>
2025-03-27 17:37:27 -0400

    used custom plane instead of sphere

--- src/components/Visualization.tsx
+++ src/components/Visualization.tsx
@@ -6,12 +6,9 @@ import {
   OrbitControls,
   PerspectiveCamera,
 } from "@react-three/drei";
-import { Leva } from 'leva'
+import { Leva } from "leva";
 import { Canvas } from "@react-three/fiber";
-import {
-  Bloom,
-  EffectComposer,
-} from "@react-three/postprocessing";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
 import { CustomSphere } from "./CustomSphere";
 import * as Tone from "tone/build/esm/index";
 import { useEffect, useRef } from "react";
@@ -19,33 +16,32 @@ import {
   useEffectsEngineStore,
   useSynthEngineStore,
 } from "@/lib/store/settingsStore";
+import React from "react";
+import { CustomPlane } from "./CustomPlane";
 
-const Scene = () => {
+export const Scene = () => {
   const color = useUiColorRGB();
   const meterRef = useRef<Tone.Meter>();
-
 
   const { effectsEngine } = useEffectsEngineStore((state) => ({
     effectsEngine: state.effectsEngine,
   }));
 
   useEffect(() => {
-    
     meterRef.current = new Tone.Meter(0.1);
     effectsEngine?.outputNode.connect(meterRef.current);
   }, []);
 
-  console.log(meterRef.current?.getValue?.());
-
   return (
     <>
-    <Leva hidden  />
       {/* <OrbitControls /> */}
-      <PerspectiveCamera fov={100} position={[0, 0, 5]} makeDefault />
-  
-    
-      <Environment background files="/envmaps/nightSky/night_sky_2k.hdr" environmentIntensity={10}
-        backgroundIntensity={0.01} 
+      <PerspectiveCamera fov={10} position={[0, 0, 20]} makeDefault />
+
+      <Environment
+        background
+        files="/envmaps/nightSky/night_sky_2k.hdr"
+        environmentIntensity={10}
+        backgroundIntensity={0.01}
       />
       <directionalLight
         color={`rgb(${color[0]}, ${color[1]}, ${color[2]})`}
@@ -58,32 +54,20 @@ const Scene = () => {
         position={[0.25, 2, -2.25]}
         receiveShadow
       />
-      
-      <CustomSphere />
-      
+
+      <CustomPlane />
+      {/* <CustomSphere /> */}
     </>
   );
 };
 
 const Visualization = () => {
-
-  
-
-  const { effectsEngine } = useEffectsEngineStore((state) => ({
-    effectsEngine: state.effectsEngine,
-  }));
-
-  const { synthEngine } = useSynthEngineStore((state) => ({
-    synthEngine: state.synthEngine,
-  }));
-
-  console.log(synthEngine)
   return (
     <div className="h-screen w-screen fixed top-0 left-0 z-0">
-      {synthEngine !== undefined && <Canvas shadows>
+      <Canvas shadows>
         <color attach="background" args={["#000000"]} />
         <Scene />
-        <EffectComposer>
+        {/* <EffectComposer>
           <Bloom
             intensity={0.2}
             blurPass={undefined}
@@ -91,9 +75,8 @@ const Visualization = () => {
             luminanceSmoothing={0.9}
             mipmapBlur={true}
           />
-        </EffectComposer>
-      </Canvas>}
-      
+        </EffectComposer> */}
+      </Canvas>
     </div>
   );
 };
